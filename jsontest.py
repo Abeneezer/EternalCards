3729f987f6f4bf94e936ce3e2a399737e4deb19c
Abeneezer <Abeneezer@gmail.com>
2018-07-04 13:40:14 +0200

    json update

--- jsontest.py
+++ jsontest.py
@@ -6,5 +6,8 @@ allNames = []
 with open('eternal-cards.json') as f:
     data = json.load(f)
     for x in range(0, len(data)):
+        if data[x]["Name"] == "Scorpion Trap":
+            pass
+            pprint (data[x])
         allNames.append([data[x]["Name"], data[x]["DetailsUrl"]])
-    pprint(allNames)
+    #pprint(data)
