4ad99c2f8051ccc03540945394b04eb5a714770e
Abeneezer <Abeneezer@gmail.com>
2018-07-20 21:32:09 +0200

    name guesser and new files

--- jsontest.py
+++ jsontest.py
@@ -1,13 +1,15 @@
 import json
+import time
 from pprint import pprint
+import difflib
 
 allNames = []
 
 with open('eternal-cards.json') as f:
     data = json.load(f)
     for x in range(0, len(data)):
-        if data[x]["Name"] == "Scorpion Trap":
-            pass
-            pprint (data[x])
-        allNames.append([data[x]["Name"], data[x]["DetailsUrl"]])
-    #pprint(data)
+        allNames.append(data[x]["Name"])
+    for x in allNames:
+        if(x == 'Rindra\'s Choice'):
+            time.sleep(1)
+            pprint(x)
