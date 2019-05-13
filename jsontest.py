5af8b4b617b2dcf3b8602daa9d669fa500d6a7d5
Abeneezer <Abeneezer@gmail.com>
2019-05-13 15:21:51 +0200

    new cards

--- jsontest.py
+++ jsontest.py
@@ -10,6 +10,4 @@ with open('eternal-cards.json') as f:
     for x in range(0, len(data)):
         allNames.append(data[x]["Name"])
     for x in allNames:
-        if(x == 'Rindra\'s Choice'):
-            time.sleep(1)
             pprint(x)
